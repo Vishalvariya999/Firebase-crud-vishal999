576d5bfbcbb979e7994473ed107d16eea0e9d03b
Vishalvariya999 <110755112+Vishalvariya999@users.noreply.github.com>
2023-08-05 09:48:23 +0530

    Create list component and services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/components/list/list.component').then(
+        (c) => c.ListComponent
+      ),
+    title: 'List',
+  },
+];
